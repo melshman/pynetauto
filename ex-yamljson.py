ed604cb689183e08674af0ae9819a5d8200e2588
Tim Armstrong <melshman@gmail.com>
2018-09-09 09:47:10 -0400

    changed to 1class structure

---
+++ ex-yamljson.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import json
+import yaml
+from pprint import pprint
+
+
+def main():
+    my_list = [
+    	'cisco', "juniper", {"users":{"username":"john", 
+    	"password":"password123"}},"nexus", {"vlans":{"vlan_name":"vlan22", "vlan_id":"22"}}, "HP"]
+    print(my_list)
+    print(my_list[0])
+    print(my_list[2].keys())
+    print(my_list[2].values())
+    print(my_list[2]["users"].keys())
+    print(my_list[2]["users"].values())
+    print(my_list[2]["users"]['username'])
+    print(my_list[2]["users"]['password'])
+    print("\n")
+
+    with open('data.yml', 'w') as outfile:
+    	yaml.dump(my_list, outfile, default_flow_style=False)
+
+    with open('data.json', 'w') as outfile:
+        json.dump(my_list, outfile)
+
+    with open('data.json','r') as f:
+        json_data = json.load(f)	
+
+    pprint(json_data)
+    print("\n")
+
+    with open('data.yml','r') as f:
+        yaml_data = yaml.load(f)
+
+    pprint(yaml_data)
+
+if __name__ == "__main__":
+	main()
