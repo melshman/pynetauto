eb50f1558eeac24765701d2ec36394abc14e0f64
Tim Armstrong <melshman@gmail.com>
2018-10-16 18:54:45 -0400

    pynet updates

--- 6class/ex6-pynxos.py
+++ 6class/ex6-pynxos.py
@@ -36,3 +36,54 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+# interface Loopback0
+#  ip address {{loopback_vars.loopback0_addr}} {{loopback_vars.loopback0_mask}}
+
+#############    help(device) ##################
+# class Device(builtins.object)
+#  |  Methods defined here:
+#  |
+#  |  __init__(self, host, username, password, transport='http', port=None, timeout=30)
+#  |      Initialize self.  See help(type(self)) for accurate signature.
+#  |
+#  |  backup_running_config(self, filename)
+#  |
+#  |  checkpoint(self, filename)
+#  |
+#  |  config(self, command)
+#  |
+#  |  config_list(self, commands)
+#  |
+#  |  feature(self, feature_name)
+#  |
+#  |  file_copy(self, src, dest=None, file_system='bootflash:')
+#  |
+#  |  file_copy_remote_exists(self, src, dest=None, file_system='bootflash:')
+#  |
+#  |  get_boot_options(self)
+#  |
+#  |  reboot(self, confirm=False)
+#  |
+#  |  rollback(self, filename)
+#  |
+#  |  save(self, filename='startup-config')
+#  |
+#  |  set_boot_options(self, image_name, kickstart=None)
+#  |
+#  |  show(self, command, raw_text=False)
+#  |
+#  |  show_list(self, commands, raw_text=False)
+#  |
+#  |  ----------------------------------------------------------------------
+#  |  Data descriptors defined here:
+#  |
+#  |  __dict__
+#  |      dictionary for instance variables (if defined)
+#  |
+#  |  __weakref__
+#  |      list of weak references to the object (if defined)
+#  |
+#  |  facts
+#  |
+#  |  running_config
