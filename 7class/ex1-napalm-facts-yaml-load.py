172e847b9159a3f7802f03a08990006607f74040
Tim Armstrong <melshman@gmail.com>
2018-10-21 21:26:52 -0400

    updates to ex1 yaml load

--- 7class/ex1-napalm-facts-yaml-load.py
+++ 7class/ex1-napalm-facts-yaml-load.py
@@ -134,23 +134,23 @@ def main():
 	for a_device in devices:
 		print(device)
 		for device_name, device_dict in devices.items():
-		print(device_name)
-		print(device_dict)
-		device_type = device_dict.pop('device_type')
-		print(device_type)
-		driver = get_network_driver(device_type)
-		# Set the password
-		device_dict['password'] = password
-		device = driver(**device_dict) 
-		napalm_conns.append(device)
-		print("\n {} device created!".format(device_dict['hostname']))
-		device.open()
-		print("\n Device connection opened of type {}!".format(device_type))
-		print("\n")
-		facts = device.get_facts()
-		model = facts['model']
-		pprint(model)
-		print("\n\n")
+			print(device_name)
+			print(device_dict)
+			device_type = device_dict.pop('device_type')
+			print(device_type)
+			driver = get_network_driver(device_type)
+			# Set the password
+			device_dict['password'] = password
+			device = driver(**device_dict) 
+			napalm_conns.append(device)
+			print("\n {} device created!".format(device_dict['hostname']))
+			device.open()
+			print("\n Device connection opened of type {}!".format(device_type))
+			print("\n")
+			facts = device.get_facts()
+			model = facts['model']
+			pprint(model)
+			print("\n\n")
 
 
 if __name__ == "__main__":
