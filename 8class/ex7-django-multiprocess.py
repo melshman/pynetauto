10926da4537cdaa7fc6f4734bb5509f2012e3695
Tim Armstrong <melshman@gmail.com>
2018-10-28 16:09:45 -0400

    ex7

--- 8class/ex7-django-multiprocess.py
+++ 8class/ex7-django-multiprocess.py
@@ -26,20 +26,22 @@ using threads?
 '''
 
 
-def show_version(a_device):
+def show_version(a_device, q):
     """Execute show version command using Netmiko."""
+    output_dict = {}
     creds = a_device.credentials
     remote_conn = ConnectHandler(device_type=a_device.device_type,
                                  ip=a_device.ip_address,
                                  username=creds.username,
                                  password=creds.password,
                                  port=a_device.port, secret='')
-    print()
-    print('#' * 80)
-    print(remote_conn.send_command_expect("show version"))
-    print('#' * 80)
-    print()
+    
+    output = ('#' * 80) + "\n" 
+    output += remote_conn.send_command_expect("show version") + "\n"
+    output = ('#' * 80) + "\n" 
 
+    output_dict[a_device.device_name] = output
+    q.put(output_dict)
 
 def main():
     """
@@ -49,15 +51,23 @@ def main():
     start_time = datetime.now()
     devices = NetworkDevice.objects.all()
     procs = []
+    q = Queue(maxsize=20)
 
     for a_device in devices:
-        my_proc = Process(target=show_version, args=(a_device,))
+        my_proc = Process(target=show_version, args=(a_device, q))
         my_proc.start()
         procs.append(my_proc)
     
+    #  Make sure all processes completed
     for a_proc in procs:
         print(a_proc)
         a_proc.join()
+
+    while not q.empty():
+    	my_dict = q.get()
+    	for k,v in my_dict.iteritems():
+    		print(k)
+    		print(v)
 
     elapsed_time = datetime.now() - start_time
     print("Elapsed time: {}".format(elapsed_time))
