baaf0a61c329145e92443cd5c7ae463389500a8b
Tim Armstrong <melshman@gmail.com>
2018-11-11 07:33:08 -0500

    bonus class begin

---
+++ bonus_jnpr_xml/ex4_pyez_intf.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+
+from getpass import getpass
+from pprint import pprint
+
+from lxml import etree
+# import xmltodict
+
+from jnpr.junos import Device
+from jnpr.junos.op.ethport import EthPortTable
+# from jnpr.junos.op.arp import ArpTable
+# from jnpr.junos.op.phyport import PhyPortTable
+# from jnpr.junos.op.phyport import PhyPortStatsTable
+# from jnpr.junos.utils.config import Config
+
+'''
+
+4. For each of the SRX's interfaces, display: the operational state, 
+packets-in, and packets-out. You will probably want to use EthPortTable 
+for this.
+
+
+'''
+
+
+def main():
+	pwd = getpass()
+
+	srx = {
+		"hostname" : "srx1.twb-tech.com",
+		"host" : "184.105.247.76",
+		"user" : "pyclass",
+		"password" : pwd
+	}
+
+	srx_device = Device(**srx)
+	srx_device.open()
+
+	print("\nThe current timeout is {} seconds.".format(srx_device.timeout))
+	srx_device.timeout = 120
+	print("\nThe updated timeout is {} seconds.".format(srx_device.timeout))
+
+	intfs = EthPortTable(srx_device)
+	pprint(intfs)
+
+
+
+	print("\nJust the intfs ma'am")
+	print("*******************")
+	pprint(intfs)
+	print("*******************\n")
+	
+	# uptime = facts['RE0']['up_time']
+	# reboot_reason = facts['re_info']['default']['0']['last_reboot_reason']
+	
+	# print("The last time this router was reboot, was {} ago and the cause was {}.\n".format(uptime, reboot_reason))
+
+
+
+if __name__ == "__main__":
+	main()
+
+
+
+'''
+looking around in facts dict
+
+In [25]: facts['fqdn']
+Out[25]: 'pynet-jnpr-srx1'
+
+In [26]: facts['re_info']
+Out[26]:
+{'default': {'0': {'mastership_state': 'master',
+   'status': 'OK',
+   'model': 'RE-SRX100H2',
+   'last_reboot_reason': '0x1:power cycle/failure'},
+  'default': {'mastership_state': 'master',
+   'status': 'OK',
+   'model': 'RE-SRX100H2',
+   'last_reboot_reason': '0x1:power cycle/failure'}}}
+
+In [27]: facts['re_info']['default']
+Out[27]:
+{'0': {'mastership_state': 'master',
+  'status': 'OK',
+  'model': 'RE-SRX100H2',
+  'last_reboot_reason': '0x1:power cycle/failure'},
+ 'default': {'mastership_state': 'master',
+  'status': 'OK',
+  'model': 'RE-SRX100H2',
+  'last_reboot_reason': '0x1:power cycle/failure'}}
+
+In [28]: facts['re_info']['default']['0']
+Out[28]:
+{'mastership_state': 'master',
+ 'status': 'OK',
+ 'model': 'RE-SRX100H2',
+ 'last_reboot_reason': '0x1:power cycle/failure'}
+
+In [29]: facts['re_info']['default']['0']['last_reboot_reason']
+Out[29]: '0x1:power cycle/failure'
+
+
+'''
+
