658c833a3e82c92ba38982cdefaac6ffd74f9255
Tim Armstrong <melshman@gmail.com>
2018-11-11 07:16:12 -0500

    bonus class begin

--- bonus_jnpr_xml/ex3_pyez_facts.py
+++ bonus_jnpr_xml/ex3_pyez_facts.py
@@ -25,70 +25,73 @@ and to print out the device's facts.
 def main():
 pwd = getpass()
 
-srx = {
-	"hostname" : "srx1.twb-tech.com",
-	"host" : "184.105.247.76",
-	"username" : "pyclass",
-	"password" : pwd
-}
+	srx = {
+		"hostname" : "srx1.twb-tech.com",
+		"host" : "184.105.247.76",
+		"user" : "pyclass",
+		"password" : pwd
+	}
 
-srx_device = Device(**srx)
-srx_device.open()
+	srx_device = Device(**srx)
+	srx_device = Device()
+	srx_device.open()
 
 	print("The current timeout is {} seconds.".format(srx_device.timeout))
 	srx_device.timeout = 120
 	print("The updated timeout is {} seconds.".format(srx_device.timeout))
 
-	facts = srx_device.get_facts()
+	facts = srx_device.facts
 
-	# print(etree.tostring(lldp, encoding='unicode', pretty_print=True).decode().rstrip())
-	# print(etree.tostring(lldp, encoding='unicode', pretty_print=True).decode())
-	print("\n")
-	print(etree.tostring(arp, encoding='unicode', pretty_print=True))
-	print("\n")
-
-	# pprint(arp.xpath("/arp-table-information/arp-table-entry/mac-address"))
-	arp_dict = {}
-	for entry in arp.xpath("/arp-table-information/arp-table-entry"):
-		for val in entry:
-			# print("{}:  {}".format(val.tag, val.text))
-			if val.tag == 'hostname':
-				host = val.text
-			elif val.tag == 'interface-name':
-				intf = val.text
-			elif val.tag == 'mac-address':
-				mac_addr = val.text
-		arp_dict.update({
-			host:{
-				'intf': intf,
-				'mac_addr': mac_addr,
-			}
-		})
-	pprint(arp_dict)
-	print("\n")
-	# pprint(lldp.getchildren())
-
-
-
-
-
-	# arp_dict = {
-	# 	local_int: {
-	# 		'remote_sys_name': remote_sys,
-	# 		'remote_port': remote_port,
-	# 	}
-	# }
-
-	# pprint(arp_dict)
-
-
-# print(lldp.getchildren())
-# print(entry1[0].text)
-# print(entry1[1].text)
-# print(lldp[0][0].tag)     ## 'lldp-local-interface'
-# print(lldp[0][0].text)   ## 'fe-0/0/7.0'
-
+	pprint(facts)
+	
+	uptime = facts['RE0']['up_time']
+	reboot_reason = facts['re_info']['default']['0']['last_reboot_reason']
+	
+	pprint("The last time this router was reboot, was {} ago and the cause was {}".format(uptime, reboot_reason))
 
 if __name__ == "__main__":
 	main()
+
+
+
+'''
+looking around in facts dict
+
+In [25]: facts['fqdn']
+Out[25]: 'pynet-jnpr-srx1'
+
+In [26]: facts['re_info']
+Out[26]:
+{'default': {'0': {'mastership_state': 'master',
+   'status': 'OK',
+   'model': 'RE-SRX100H2',
+   'last_reboot_reason': '0x1:power cycle/failure'},
+  'default': {'mastership_state': 'master',
+   'status': 'OK',
+   'model': 'RE-SRX100H2',
+   'last_reboot_reason': '0x1:power cycle/failure'}}}
+
+In [27]: facts['re_info']['default']
+Out[27]:
+{'0': {'mastership_state': 'master',
+  'status': 'OK',
+  'model': 'RE-SRX100H2',
+  'last_reboot_reason': '0x1:power cycle/failure'},
+ 'default': {'mastership_state': 'master',
+  'status': 'OK',
+  'model': 'RE-SRX100H2',
+  'last_reboot_reason': '0x1:power cycle/failure'}}
+
+In [28]: facts['re_info']['default']['0']
+Out[28]:
+{'mastership_state': 'master',
+ 'status': 'OK',
+ 'model': 'RE-SRX100H2',
+ 'last_reboot_reason': '0x1:power cycle/failure'}
+
+In [29]: facts['re_info']['default']['0']['last_reboot_reason']
+Out[29]: '0x1:power cycle/failure'
+
+
+'''
 
